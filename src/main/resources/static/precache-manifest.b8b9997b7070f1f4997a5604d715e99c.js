8da7725cad8d2483dab63d19f8fd5e14b4f35398
tkzhakuna <tkzhaku@gmail.com>
2020-03-10 13:46:17 +0200

    React added

---
+++ src/main/resources/static/precache-manifest.b8b9997b7070f1f4997a5604d715e99c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9d8c8076f2af5240657417b265badd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5ca0575938bab2ea84a",
+    "url": "/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "507695f6d81eed01b5fe",
+    "url": "/static/css/main.53a6e59f.chunk.css"
+  },
+  {
+    "revision": "f5ca0575938bab2ea84a",
+    "url": "/static/js/2.33fd32bc.chunk.js"
+  },
+  {
+    "revision": "7cba650847d31a42aafcd9fcab1c9372",
+    "url": "/static/js/2.33fd32bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507695f6d81eed01b5fe",
+    "url": "/static/js/main.b5faf67b.chunk.js"
+  },
+  {
+    "revision": "c081e2f36df4981bab2b",
+    "url": "/static/js/runtime-main.8b4b340c.js"
+  }
+]);
